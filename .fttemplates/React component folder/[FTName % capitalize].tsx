e1c1ea1288ddb24cb22b6b4ab8ecac27c07b44f1
igor.lednev <igor.lednev@quantumsoft.ru>
2022-05-03 13:48:55 +0700

    Revert "- templates"
    This reverts commit 8b7ef8f720046f69d532a73d130e5f29579470f2.

---
+++ .fttemplates/React component folder/[FTName % capitalize].tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './[FTName % capitalize].module.css';
+
+interface <FTName | capitalize>Props {}
+
+const <FTName | capitalize>: React.FC<<FTName | capitalize>Props> = () => <div className={styles.<FTName | lowercasefirstchar>}><FTName | capitalize></div>;
+
+export default <FTName | capitalize>;
